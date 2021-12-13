76609f41b5db2f87908ffe1c68cad2932c0e57bb
Felix Fontein <felix@fontein.de>
2021-12-13 18:44:52 +0100

    Use src subdirectory. (#71)

--- lint-flake8.sh
+++ lint-flake8.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-poetry run flake8 antsibull_changelog --count --max-complexity=10 --max-line-length=100 --statistics "$@"
+poetry run flake8 src/antsibull_changelog --count --max-complexity=10 --max-line-length=100 --statistics "$@"
