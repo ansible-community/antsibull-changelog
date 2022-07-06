855e141532ff346dd5021a21b36d22cdce14068f
Felix Fontein <felix@fontein.de>
2022-07-06 07:57:45 +0200

    Support rstcheck_core via rstcheck 6.x.

---
+++ src/antsibull_changelog/rstcheck.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Author: Felix Fontein <felix@fontein.de>
+# License: GPLv3+
+# Copyright: Ansible Project, 2022
+
+"""
+Handle rstcheck.
+"""
+
+import os.path
+import pathlib
+import tempfile
+import typing as t
+
+# rstcheck >= 6.0.0 depends on rstcheck-core
+try:
+    import rstcheck_core.checker
+    import rstcheck_core.config
+    HAS_RSTCHECK_CORE = True
+except ImportError:
+    HAS_RSTCHECK_CORE = False
+    import docutils.utils
+    import rstcheck
+
+
+def check_rst_content(content: str, filename: t.Optional[str] = None
+                      ) -> t.List[t.Tuple[int, int, str]]:
+    '''
+    Check the content with rstcheck. Return list of errors and warnings.
+
+    The entries in the return list are tuples with line number, column number, and
+    error/warning message.
+    '''
+    if HAS_RSTCHECK_CORE:
+        filename = os.path.basename(filename or 'file.rst') or 'file.rst'
+        with tempfile.TemporaryDirectory() as tempdir:
+            rst_path = os.path.join(tempdir, filename)
+            with open(rst_path, 'w', encoding='utf-8') as f:
+                f.write(content)
+            config = rstcheck_core.config.RstcheckConfig(
+                report_level=rstcheck_core.config.ReportLevel.WARNING,
+            )
+            core_results = rstcheck_core.checker.check_file(pathlib.Path(rst_path), config)
+            return [(result.line_number, 0, result.message) for result in core_results]
+    else:
+        results = rstcheck.check(
+            content,
+            filename=filename,
+            report_level=docutils.utils.Reporter.WARNING_LEVEL,
+        )
+        return [(result[0], 0, result[1]) for result in results]
