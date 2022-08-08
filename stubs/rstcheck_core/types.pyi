4691d265e22a6f51899db7756d803f494abbf35c
Felix Fontein <felix@fontein.de>
2022-08-08 21:09:38 +0200

    Fix rstcheck-core support (#83)
    * Fix rstcheck-core support.
    
    * Try to trick poetry to install newer rstcheck.
    
    * Fix missing whitespace.
    
    * Fix linting false positive.
    
    * During testing (not linting), modify Python restrictions in pyproject.yml.
    
    * Fix wrong version number.

--- stubs/rstcheck_core/types.pyi
+++ stubs/rstcheck_core/types.pyi
@@ -3,8 +3,13 @@ import pathlib
 
 from typing import Literal, Union
 
+try:
+    from typing import TypedDict
+except ImportError:
+    from typing_extensions import TypedDict
 
-class LintError:
+
+class LintError(TypedDict):
     source_origin: Union[pathlib.Path, Literal["<string>"], Literal["<stdin>"]]
     line_number: int
     message: str
