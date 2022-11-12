8c8bb42de86278478134f1e9952560d921bf0f2f
Felix Fontein <felix@fontein.de>
2022-11-12 15:55:01 +0100

    Replace distutils.sysconfig with sysconfig. distutils is deprecated and will be removed in Python 3.12.

--- lint-pyre.sh
+++ lint-pyre.sh
@@ -4,4 +4,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 set -e
-poetry run pyre --source-directory src --search-path $(poetry run python -c 'from distutils.sysconfig import get_python_lib;print(get_python_lib())') --search-path stubs/ --search-path . check "$@"
+
+PURELIB=$(poetry run python -c 'import sysconfig; print(sysconfig.get_path("purelib"))')
+PLATLIB=$(poetry run python -c 'import sysconfig; print(sysconfig.get_path("platlib"))')
+poetry run pyre --source-directory src --search-path "$PURELIB" --search-path "$PLATLIB" --search-path stubs/ "$@"
