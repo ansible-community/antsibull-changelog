397cdd7c719812e004dada4b2a5c37f614b09749
Sagar Paul <sagpaul@redhat.com>
2023-07-06 10:47:58 +0530

    Update src/antsibull_changelog/config.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- src/antsibull_changelog/config.py
+++ src/antsibull_changelog/config.py
@@ -302,12 +302,11 @@ class CollectionDetails:
         """
         Load and update galaxy.yaml file.
         """
-        if self.paths.galaxy_path:
-            config = load_yaml(self.paths.galaxy_path)
-            config["version"] = str(version)
-            store_yaml(self.paths.galaxy_path, config)
-        else:
+        if not self.paths.galaxy_path:
             raise ChangelogError("Cannot find galaxy.yml file in path.")
+        config = load_yaml(self.paths.galaxy_path)
+        config["version"] = str(version)
+        store_yaml(self.paths.galaxy_path, config)
 
 
 DEFAULT_SECTIONS = [
