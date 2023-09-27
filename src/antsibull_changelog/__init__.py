65528fa0551f8743739b84fe143a31d7a2cffb5c
Felix Fontein <felix@fontein.de>
2023-09-27 19:54:41 +0200

    Use 'hatch version' instead of manually updating the version.

--- src/antsibull_changelog/__init__.py
+++ src/antsibull_changelog/__init__.py
@@ -3,3 +3,11 @@
 # https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2020, Ansible Project
+
+"""
+Ansible Changelog Tool.
+"""
+
+__version__ = "0.23.0.post0"
+
+__all__ = ("__version__",)
