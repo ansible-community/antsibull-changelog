238d5660c14b68e14b201c017f2700c00ef7024b
Maxwell G <maxwell@gtmx.me>
2024-02-13 23:44:51 +0000

    nox bump: fix extra newlines in generated release fragment

--- noxfile.py
+++ noxfile.py
@@ -246,11 +246,12 @@ def bump(session: nox.Session):
         fragment = session.run(
             "python",
             "-c",
-            f"import yaml ; print(yaml.dump(dict(release_summary={repr(session.posargs[1])})))",
+            "import sys, yaml ;"
+            f" yaml.dump(dict(release_summary={repr(session.posargs[1])}), sys.stdout)",
             silent=True,
         )
         with open(fragment_file, "w") as fp:
-            print(fragment, file=fp)
+            fp.write(fragment)
         session.run(
             "git",
             "add",
