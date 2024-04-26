2719e87412d2d43e4e15c15e41ed51c7ccae4ed6
gardar <gardar@users.noreply.github.com>
2024-04-26 17:48:10 +0000

    fix: Increase indent of list items in yaml output
    Signed-off-by: gardar <gardar@users.noreply.github.com>

--- src/antsibull_changelog/yaml.py
+++ src/antsibull_changelog/yaml.py
@@ -15,14 +15,19 @@ from typing import Any
 import yaml
 
 _SafeLoader: Any
-_SafeDumper: Any
 try:
     # use C version if possible for speedup
-    from yaml import CSafeDumper as _SafeDumper
     from yaml import CSafeLoader as _SafeLoader
 except ImportError:
-    from yaml import SafeDumper as _SafeDumper
     from yaml import SafeLoader as _SafeLoader
+
+
+class _SafeDumper(yaml.SafeDumper):
+    """
+    Extend YAML dumper to increase indent of list items.
+    """
+    def increase_indent(self, flow=False, indentless=False):
+        return super().increase_indent(flow, False)
 
 
 def load_yaml(path: str) -> Any:
@@ -38,12 +43,10 @@ def store_yaml(path: str, content: Any) -> None:
     Store ``content`` as YAML file under ``path``.
     """
     with open(path, "w", encoding="utf-8") as stream:
-        dumper = _SafeDumper
-        dumper.ignore_aliases = lambda *args: True
         yaml.dump(
             content,
             stream,
             default_flow_style=False,
-            Dumper=dumper,
+            Dumper=_SafeDumper,
             explicit_start=True
         )
