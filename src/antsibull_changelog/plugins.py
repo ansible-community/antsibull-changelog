9a87bc1784fde82ff9dcd53657a3a52803337d72
Felix Fontein <felix@fontein.de>
2024-08-25 22:19:38 +0200

    Move directory and collection copier code to copier.py.

--- src/antsibull_changelog/plugins.py
+++ src/antsibull_changelog/plugins.py
@@ -14,9 +14,7 @@ from __future__ import annotations
 import json
 import os
 import re
-import shutil
 import subprocess
-import tempfile
 from typing import Any
 
 import packaging.version
@@ -28,8 +26,8 @@ from .ansible import (
     get_documentable_plugins,
 )
 from .config import ChangelogConfig, CollectionDetails, PathsConfig
+from .copier import CollectionCopier
 from .logger import LOGGER
-from .utils import detect_vcs
 from .yaml import load_yaml, store_yaml
 
 
@@ -371,115 +369,6 @@ def load_plugin_metadata(  # pylint: disable=too-many-arguments
         )
         result[processed_data["name"]] = processed_data
     return result
-
-
-class Copier:  # pylint: disable=too-few-public-methods
-    """
-    Allows to copy directories.
-    """
-
-    def copy(self, from_path: str, to_path: str) -> None:
-        """
-        Copy a directory ``from_path`` to a destination ``to_path``.
-
-        ``to_path`` must not exist, but its parent directory must exist.
-        """
-        LOGGER.debug("Copying complete directory from {!r} to {!r}", from_path, to_path)
-        shutil.copytree(from_path, to_path, symlinks=True)
-
-
-class GitCopier(Copier):  # pylint: disable=too-few-public-methods
-    """
-    Allows to copy directories that are part of a Git repository.
-    """
-
-    def copy(self, from_path: str, to_path: str) -> None:
-        LOGGER.debug("Identifying files not ignored by Git in {!r}", from_path)
-        os.mkdir(to_path, mode=0o700)
-        files = (
-            subprocess.check_output(
-                [
-                    "git",
-                    "ls-files",
-                    "-z",
-                    "--cached",
-                    "--others",
-                    "--exclude-standard",
-                    "--deduplicate",
-                ],
-                cwd=from_path,
-            )
-            .strip(b"\x00")
-            .split(b"\x00")
-        )
-
-        LOGGER.debug(
-            "Copying {} file(s) from {!r} to {!r}", len(files), from_path, to_path
-        )
-        created_directories = set()
-        for file in files:
-            # Decode filename and check whether the file still exists
-            # (deleted files are part of the output)
-            file_decoded = file.decode("utf-8")
-            src_path = os.path.join(from_path, file_decoded)
-            if not os.path.exists(src_path):
-                continue
-
-            # Check whether the directory for this file exists
-            directory, _ = os.path.split(file_decoded)
-            if directory not in created_directories:
-                os.makedirs(os.path.join(to_path, directory), mode=0o700, exist_ok=True)
-                created_directories.add(directory)
-
-            # Copy the file
-            dst_path = os.path.join(to_path, file_decoded)
-            shutil.copyfile(src_path, dst_path)
-
-
-class CollectionCopier:
-    """
-    Creates a copy of a collection to a place where ``--playbook-dir`` can be used
-    to prefer this copy of the collection over any installed ones.
-    """
-
-    def __init__(
-        self, paths: PathsConfig, config: ChangelogConfig, namespace: str, name: str
-    ):
-        self.paths = paths
-        self.namespace = namespace
-        self.name = name
-        self.copier = Copier()
-        vcs = config.vcs
-        if vcs == "auto":
-            vcs = detect_vcs(self.paths.base_dir)
-        if vcs == "git":
-            self.copier = GitCopier()
-
-        self.dir = os.path.realpath(tempfile.mkdtemp(prefix="antsibull-changelog"))
-
-    def __enter__(self):
-        try:
-            collection_container_dir = os.path.join(
-                self.dir, "collections", "ansible_collections", self.namespace
-            )
-            os.makedirs(collection_container_dir)
-
-            collection_dir = os.path.join(collection_container_dir, self.name)
-            LOGGER.debug("Temporary collection directory: {!r}", collection_dir)
-
-            self.copier.copy(self.paths.base_dir, collection_dir)
-
-            LOGGER.debug("Temporary collection directory has been populated")
-            new_paths = PathsConfig.force_collection(
-                collection_dir, ansible_doc_bin=self.paths.ansible_doc_path
-            )
-            return self.dir, new_paths
-        except Exception:
-            shutil.rmtree(self.dir, ignore_errors=True)
-            raise
-
-    def __exit__(self, type_, value, traceback_):
-        shutil.rmtree(self.dir, ignore_errors=True)
 
 
 def _load_plugins_2_13(
